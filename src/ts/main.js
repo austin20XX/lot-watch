981081571577cefbf7495bccfa02f6bcdaf29d84
austin_m <1austinm@protonmail.com>
2024-10-07 15:01:40 -0500

    chore: prettier formatting

--- src/ts/main.js
+++ src/ts/main.js
@@ -1,8 +1,9 @@
-// { 
-    let map = L.map('property-map').setView({lat: 41.816651, lng: -87.616797}, 16);
+// {
+let map = L.map('property-map').setView({lat: 41.816651, lng: -87.616797}, 16);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
 // } Uncomment to save variables from global scope
