fb65c98778a5cc316af1f819b0f1622602cfa166
austin_m <1austinm@protonmail.com>
2024-10-17 11:53:29 -0500

    feat: convert to typescript

---
+++ src/ts/main.ts
@@ -0,0 +1,31 @@
+// {
+import { map, Map, latLng, tileLayer, MapOptions, geoJSON } from 'leaflet'; 
+import { illinoisZCTA } from '../assets/il_illinois_zip_codes_geo';
+
+const mapOptions: MapOptions = { center: latLng(41.816651, -87.616797), zoom: 16 };
+let zipMap: Map = map('property-map', mapOptions);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map);
+
+//  Water color tile layer
+tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
+	minZoom: 1,
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(zipMap);
+
+geoJSON(illinoisZCTA).addTo(zipMap);
+
+
+
+// var Thunderforest_TransportDark = L.tileLayer('https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey={apikey}', {
+// 	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// 	apikey: '<your apikey>',
+// 	maxZoom: 22
+// });
+
+// } Uncomment to save variables from global scope
